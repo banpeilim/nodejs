ba219e84bdaf1b0a1080dec0864eb364149df628
BanPei Lim <banpei.lim@gmail.com>
2023-02-25 22:08:39 +0800

    Second Commit

--- src/api.js
+++ src/api.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
   });
 });
 
-app.use(`/name/api`, router);
+app.use(`/.netlify/functions/api`, router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
