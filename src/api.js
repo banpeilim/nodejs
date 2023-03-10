c495f621e64c14212c89531ec18d58825ff6f4ad
BanPei Lim <banpei.lim@gmail.com>
2023-03-10 11:01:36 +0800

    commit

--- src/api.js
+++ src/api.js
@@ -10,6 +10,37 @@ router.get("/", (req, res) => {
   });
 });
 
+router.get("/lighton", (req, res) => {
+  var bridgeIp = "192.168.1.108";
+
+  // Set the ID of the light you want to control
+  var lightId = "1";
+
+  // Set the API key for your Hue bridge
+  var apiKey = "HIjG0NftLDjrWmp45y-8Ew7u0HrOKXGz6MgQmBIA";
+
+  // Function to turn the light on
+  function turnOn() {
+    $.ajax({
+      type: "PUT",
+      url:
+        "http://" +
+        bridgeIp +
+        "/api/" +
+        apiKey +
+        "/lights/" +
+        lightId +
+        "/state",
+      data: '{"on":true}',
+      success: function () {
+        console.log("Light turned on.");
+      },
+    });
+  }
+
+  turnOn();
+});
+
 app.use(`/.netlify/functions/api/name`, router);
 
 module.exports = app;
